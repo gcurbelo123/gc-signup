8cb13ac66936f1ce51923f15c354361617f5e4f2
gcurbelo123 <gcurbelo@csumb.edu>
2018-04-29 22:43:19 -0700

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -5,10 +5,24 @@
         $password = "";
         $dbname = $db; 
         
-        // Create connection
-        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $charset = 'utf8mb4';
     
-        return $dbConn; 
+        //checking whether the URL contains "herokuapp" (using Heroku)
+        if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+           $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+           $host = $url["host"];
+           $db   = substr($url["path"], 1);
+           $user = $url["user"];
+           $pass = $url["pass"];
+        }
+        
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        $pdo = new PDO($dsn, $user, $pass, $opt);
+        return $pdo;
     }
 ?>
